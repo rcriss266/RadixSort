c6b37c5bcb136bfb1230e0df86b92feafe559daa
rcriss266 <rcriss266@gmail.com>
2025-11-19 12:46:05 -0600

    commit se me fue el internet

--- RadixSort.java
+++ RadixSort.java
@@ -13,5 +13,31 @@ public class RadixSort {
         }
         return max;
     }
+        
+    public static void countingSort(int[] arr, int exp) {
+        int n = arr.length;
+        int[] output = new int[n];
+        int[] count = new int[10];
+
+        for (int i = 0; i < n; i++) {
+            int digit = (arr[i] / exp) % 10;
+            count[digit]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            int digit = (arr[i] / exp) % 10;
+            output[count[digit] - 1] = arr[i];
+            count[digit]--;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = output[i];
+        }
+    }
+
 
 }
