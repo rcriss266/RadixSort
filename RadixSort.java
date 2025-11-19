ff9dd39c42f1684ff5537b150feaee1713be8665
rcriss266 <rcriss266@gmail.com>
2025-11-19 12:48:55 -0600

    Tercer commit

--- RadixSort.java
+++ RadixSort.java
@@ -1,6 +1,16 @@
 public class RadixSort {
 
     public static void main(String[] args) {
+                int[] arr = {170, 45, 75, 90, 802, 24, 2, 66};
+
+        System.out.println("Antes de ordenar:");
+        for (int num : arr) System.out.print(num + " ");
+
+        radixSort(arr);
+
+        System.out.println("\nDespués de ordenar:");
+        for (int num : arr) System.out.print(num + " ");
+
 
     }
         // Obtener el valor máximo del arreglo
@@ -38,6 +48,16 @@ public class RadixSort {
             arr[i] = output[i];
         }
     }
+        
+    public static void radixSort(int[] arr) {
+        int max = getMax(arr);
+
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countingSort(arr, exp);
+        }
+    }
+    
+
 
 
 }
